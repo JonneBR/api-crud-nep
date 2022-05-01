3373e63c87aa2c76cd3adaa9cacab2059ece2c69
unknown <joaomgoncalvesbr@gmail.com>
2022-05-01 15:58:15 -0300

    all http principal methods working on tests

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -4,8 +4,16 @@ exports.getPosts = function () {
   return postsData.getPosts();
 };
 
+exports.getPost = function (id) {
+  return postsData.getPost(id);
+};
+
 exports.savePost = function (post) {
   return postsData.savePost(post);
+};
+
+exports.updatePost = function (id, post) {
+  return postsData.updatePost(id, post);
 };
 
 exports.deletePost = function (id) {
--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -26,7 +26,7 @@ test("Should get posts", async function () {
   await postsService.deletePost(post1.id);
 });
 
-test("Should save post", async function () {
+test("Should save a post", async function () {
   const data = { title: generate(), content: generate() };
   const response = await request("http://localhost:3333/posts", "post", data);
   const post = response.data;
@@ -35,4 +35,32 @@ test("Should save post", async function () {
   expect(post.content).toBe(data.content);
 
   await postsService.deletePost(post.id);
+});
+
+test("Should update a post", async function () {
+  const post = await postsService.savePost({
+    title: generate(),
+    content: generate(),
+  });
+  post.title = generate();
+  post.content = generate();
+  await request(`http://localhost:3333/posts/${post.id}`, "put", post);
+
+  const updatePost = await postsService.getPost(post.id);
+
+  expect(updatePost.title).toBe(post.title);
+  expect(updatePost.content).toBe(post.content);
+
+  await postsService.deletePost(post.id);
+});
+
+test("Should delete a post", async function () {
+  const post = await postsService.savePost({
+    title: generate(),
+    content: generate(),
+  });
+
+  await request(`http://localhost:3333/posts/${post.id}`, "delete");
+  const posts = await postsService.getPosts();
+  expect(posts).toHaveLength(0);
 });
